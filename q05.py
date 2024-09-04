9c650dc7cf6737f88cfc6499993e666425eaf099
Joao <146024681+jcandido12@users.noreply.github.com>
2024-09-04 11:44:23 -0300

    Update q05.py

--- q05.py
+++ q05.py
@@ -1,12 +1,9 @@
-# Função para inverter uma string
 def inverter_string(s):
     string_invertida = ""
     for char in s:
         string_invertida = char + string_invertida
     return string_invertida
 
-# Entrada do usuário
 entrada = input("Digite a string a ser invertida: ")
 
-# Inverter a string e exibir o resultado
 print(f"String invertida: {inverter_string(entrada)}")

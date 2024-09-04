d648af2bd2e672a824715a6690d9b8c1b519bfa2
Joao <146024681+jcandido12@users.noreply.github.com>
2024-09-04 11:41:08 -0300

    Questoes estagio ADS
    Questoes estagio Analise e Desenvolvimento de Sistemas

---
+++ q04.py
@@ -0,0 +1,16 @@
+# Valores de faturamento por estado
+faturamento = {
+    "SP": 67836.43,
+    "RJ": 36678.66,
+    "MG": 29229.88,
+    "ES": 27165.48,
+    "Outros": 19849.53
+}
+
+# Cálculo do faturamento total
+faturamento_total = sum(faturamento.values())
+
+# Exibição dos percentuais
+for estado, valor in faturamento.items():
+    percentual = (valor / faturamento_total) * 100
+    print(f"{estado}: {percentual:.2f}%")
